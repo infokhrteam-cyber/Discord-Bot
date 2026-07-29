4d66273538ca939b58497a00acce6c129faa9f81
infokhrteam-cyber <infokhrteam@gmail.com>
2026-07-30 02:59:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -320,7 +320,7 @@ client.on('messageCreate', async (message) => {
 
         try {
             // Gemini API (1.5 Flash) ko request bhejna
-            const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
+            const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
             
             const response = await fetch(url, {
                 method: 'POST',
